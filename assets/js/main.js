804d83d124ebe40fc0936ab123673fa1d6ef6db0
Ferry Oktariansyah <blaskishare@gmail.com>
2025-05-10 00:41:41 +0700

    refactor the portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@ const menu = document.getElementById("menu");
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+document.querySelectorAll("#menu a").forEach((item) => {
+  item.addEventListener("click", () => {
+    menu.classList.add("hidden");
+  });
+});
